6e8fcaf70c8042e5c02463b03de838c65eae2b27
Methush Anjula <methushanjula01@gmail.com>
2025-04-30 22:06:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.js'],
+    css: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  }
 });
