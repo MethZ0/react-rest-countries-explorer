30b187d0dcf116d6e201a79c2a1006dcd0f7afe1
Methush Anjula <methushanjula01@gmail.com>
2025-05-02 04:59:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import CountryDetailPage from "./pages/CountryDetailPage"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import FavoritesPage from "./pages/FavoritesPage"
+import LandingPage from "./pages/LandingPage"
 import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-black text-white">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 pt-16"> {/* Added pt-16 for padding-top */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/explore" element={<HomePage />} />
           <Route path="/country/:code" element={<CountryDetailPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
